ec28493083b8c0b772a79237f8f794f5114f1489
steffi-2004 <steffimiranda2004@gmail.com>
2026-02-25 22:41:26 +0530

    Add files via upload
    git commit -m "feat(ui): implement hero and feature sections from Figma design"

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import HeroSection from "./components/Hero";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
